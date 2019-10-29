b77a4d354f21c9bf0874e8891ac3d55903b3bdcc
Angelika990 <angelika-jakel@gmx.de>
2019-10-29 15:57:52 +0100

    Farben

---
+++ web.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+
+  let rotIn = document.getElementById("rot");
+  let orangeIn = document.getElementById("orange");
+  let gelbIn = document.getElementById("gelb");
+  let gruenIn = document.getElementById("gruen");
+  let blauIn = document.getElementById("blau");
+  let indigoIn = document.getElementById("indigo");
+  let gruenIn = document.getElementById("gruen");
+
+}
