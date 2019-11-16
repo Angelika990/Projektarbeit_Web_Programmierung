14fb8b55fa7cd7acf899bdf3c79a187ef2649941
Celina Maihack <celina.maihack@hunkler.de>
2019-11-16 22:31:18 +0100

    Abgabe

--- Homepage/filme/Seite1.js
+++ Homepage/filme/Seite1.js
@@ -12,7 +12,7 @@ x.style.display = "block";
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
-    x.style.display = "none"; //hide the element
+    x.style.display = "none"; //verstecken
   }
 }
 function showDiv() {
@@ -32,11 +32,11 @@ for (i = 0; i < dieFilmliste.length; i++) {
 }
 
 
-// Create a new list item when clicking on the "Add" button
+// Neuer Listeneintrag erstellen
 function neuerEintrag() {
   var li = document.createElement("li");
 
-//Hier wird die Eingabe für Titel ausgewertet
+//Hier wird beschrieben, in welcher Form der fertige Eintrag angezeigt werden soll
     var q = document.createTextNode("Titel: ");
     var inputValue = document.getElementById("titel").value;
     var r = document.createElement("br");
@@ -109,7 +109,7 @@ function neuerEintrag() {
 
 
 
-
+//Hier wird sichergestellt, dass alle Felder ausgefüllt wurden
 
   if (inputValue === '') {
     alert("Du musst alle Felder bearbeiten!");
@@ -127,7 +127,7 @@ function neuerEintrag() {
   document.getElementById("linkZumTrailer").value = "";
   document.getElementById("sprache").value = "";
 
-  // Create a "close" button and append it to each list item
+  // Lösch-Button wird zu jedem Eintrag hinzugefügt
   var dieListe = document.getElementsByTagName("LI");
   var i;
   for (i = 0; i < dieListe.length; i++) {
@@ -138,7 +138,7 @@ function neuerEintrag() {
    dieListe[i].appendChild(span);
   }
 
-  // Click on a close button to hide the current list item
+  // Funktion des Lösch-Button
   var close = document.getElementsByClassName("close");
   var i;
   for (i = 0; i < close.length; i++) {
@@ -149,159 +149,4 @@ function neuerEintrag() {
    }
   }
 
-
-
-
-  //verantwortlich für das ein- und ausschalten des Eingabefeldes
-
-
-  function plusButton() {
-    var x = document.getElementById("sometimesThere");
-  x.style.display = "block";
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none"; //hide the element
-    }
-  }
-  function showDiv() {
-     document.getElementById('sometimesThere').style.display = "block";
-  }
-
-  // ab hier beginnt der Film-Einträgungsteil
-
-  var dieFilmliste = document.getElementsByTagName("sometimesThere");
-  var i;
-  for (i = 0; i < dieFilmliste.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    dieFilmliste[i].appendChild(span);
-  }
-
-
-  // Create a new list item when clicking on the "Add" button
-  function neuerEintrag() {
-    var li = document.createElement("li");
-
-  //Hier wird die Eingabe für Titel ausgewertet
-      var q = document.createTextNode("Titel: ");
-      var inputValue = document.getElementById("titel").value;
-      var r = document.createElement("br");
-      var a = document.createTextNode(inputValue);
-      li.appendChild(q);
-      li.appendChild(a);
-      li.appendChild(r);
-
-    var j = document.createTextNode("Erscheinungsjahr: ");
-    var inputValue = document.getElementById("erscheinungsjahr").value;
-    var s = document.createElement("br");
-    var b = document.createTextNode(inputValue);
-    li.appendChild(j);
-    li.appendChild(b);
-    li.appendChild(s);
-
-    var k = document.createTextNode("Genre: ");
-    var inputValue = document.getElementById("genre").value;
-    var t = document.createElement("br");
-    var c = document.createTextNode(inputValue);
-    li.appendChild(k);
-    li.appendChild(c);
-    li.appendChild(t);
-
-
-    var l = document.createTextNode("FSK: ");
-    var inputValue = document.getElementById("fsk").value;
-    var u = document.createElement("br");
-    var d = document.createTextNode(inputValue);
-    li.appendChild(l);
-    li.appendChild(d);
-    li.appendChild(u);
-
-
-    var m = document.createTextNode("Filmlänge: ");
-    var inputValue = document.getElementById("filmlänge").value;
-    var v = document.createElement("br");
-    var e = document.createTextNode(inputValue);
-    li.appendChild(m);
-    li.appendChild(e);
-    li.appendChild(v);
-
-
-    var n = document.createTextNode("Eigene Bewertung: ");
-    var inputValue = document.getElementById("eigeneBewertung").value;
-    var w = document.createElement("br");
-    var f = document.createTextNode(inputValue);
-    li.appendChild(n);
-    li.appendChild(f);
-    li.appendChild(w);
-
-
-    var o = document.createTextNode("Link zum Trailer: ");
-    var inputValue = document.getElementById("linkZumTrailer").value;
-    var x = document.createElement("br");
-    var g = document.createTextNode(inputValue);
-    li.appendChild(o);
-    li.appendChild(g);
-    li.appendChild(x);
-
-
-    var p = document.createTextNode("Sprache: ");
-    var inputValue = document.getElementById("sprache").value;
-    var y = document.createElement("br");
-    var h = document.createTextNode(inputValue);
-    li.appendChild(p);
-    li.appendChild(h);
-    li.appendChild(y);
-
-
-
-
-
-
-    if (inputValue === '') {
-      alert("Du musst alle Felder bearbeiten!");
-    } else {
-      document.getElementById("filmeListe").appendChild(li);
-    }
-
-
-    document.getElementById("titel").value = "";
-    document.getElementById("erscheinungsjahr").value = "";
-    document.getElementById("genre").value = "";
-    document.getElementById("fsk").value = "";
-    document.getElementById("filmlänge").value = "";
-    document.getElementById("eigeneBewertung").value = "";
-    document.getElementById("linkZumTrailer").value = "";
-    document.getElementById("sprache").value = "";
-
-    //Löschen-Button erstellen
-    var dieFilmliste = document.getElementsByTagName("li");
-    var i;
-    for (i = 0; i < dieFilmliste.length; i++) {
-      var span = document.createElement("SPAN");
-      var txt = document.createTextNode("\u00D7");
-      span.className = "close";
-      span.appendChild(txt);
-      dieFilmliste[i].appendChild(span);
-    }
-
-
-  }
-
-
-
-
-
-// Click on a close button to hide the current list item
-function close(){
-var close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
- close[i].onClick = function() {
-   var div = this.parentElement;
-   div.style.display = "none";
- }
-}
-}
 }
