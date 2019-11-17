3ff9ab6771bf4d95d12a189ee1809fb5b5a500e3
Ann-Marie Krems <mariechen010@hotmail.de>
2019-11-17 12:43:37 +0100

    zusammenführung

--- Homepage/appmain.js
+++ Homepage/appmain.js
@@ -1,70 +1,69 @@
+function changePage(targetId) {
+  let pages = document.querySelectorAll('.page');
+  pages.forEach(el => {
+    if (el.id == targetId) {
+      el.classList.add('active')
+    }
+    else {
+      el.classList.remove('active');
+    }
+  })
+
+  history.pushState({}, targetId, `#${targetId}`);
+}
+
+let bildnummer = 1;
+function changeBackground() {
+  // Setzen der Bildnummer, auslesen aller zu ändender Elemente,
+  //  und Zuweisung eines Eventlisteners,
+  // der die Funktion ausführt zur Änderung des Layouts.
+
+  let serienKopf = document.getElementById("serienkopf");
+  let allesumschliessend = document.getElementById("alles");
+  let ueberschriftSerie= document.getElementById("ueberschriftSerie");
+  let ueberschriftMusik= document.getElementById("musikUeberschrift");
+  // Überprüfung welches Thema aktuell ausgewählt ist
+  // Ändern des Hintergrund zum nächsten Thema
+  if(bildnummer == "1") {
+    serienKopf.style.backgroundImage="url(./serien/serienTop2.png)";
+    allesumschliessend.style.backgroundImage = "url(./serien/background2.png)";
+    ueberschriftSerie.style.color = "white";
+    ueberschriftMusik.style.color = "black";
+    bildnummer=2;
+  }
+  else if (bildnummer== "2"){
+
+    serienKopf.style.backgroundImage="url(./serien/serienTop3.png)";
+    allesumschliessend.style.backgroundImage = "url(./serien/background3.png)";
+    bildnummer=3;
+  }
+  else if (bildnummer== "3"){
+
+    serienKopf.style.backgroundImage="url(./serien/serienTop4.png)";
+    allesumschliessend.style.backgroundImage = "url(./serien/background4.png)";
+    bildnummer=4;
+  }
+  else if (bildnummer== "4"){
+
+    serienKopf.style.backgroundImage="url(./serien/serienTop5.png)";
+    allesumschliessend.style.backgroundImage = "url(./serien/background5.png)";
+    bildnummer=5;
+  }
+  else if (bildnummer== "5"){
+
+    serienKopf.style.backgroundImage="url(./serien/serienTop.png)";
+    allesumschliessend.style.backgroundImage = "url(./serien/background.png)";
+    ueberschriftSerie.style.color = "black";
+    ueberschriftMusik.style.color = "white";
+    bildnummer=1;
+  }
+}
+
+window.addEventListener('load', function() {
+  history.replaceState({}, 'serien', '#serien');
+})
+
 const app = {
-    pages: [],
-    show: new Event('show'),
-    init: function(){
-        app.pages = document.querySelectorAll('.page');
-        app.pages.forEach((pg)=>{
-            pg.addEventListener('show', app.pageShown);
-        })
-
-        document.querySelectorAll('.nav-link').forEach((link)=>{
-            link.addEventListener('click', app.nav);
-        })
-        history.replaceState({}, 'serien', '#serien');
-        window.addEventListener('popstate', app.poppin);
-
-        // Setzen der Bildnummer, auslesen aller zu ändender Elemente,
-        //  und Zuweisung eines Eventlisteners,
-        // der die Funktion ausführt zur Änderung des Layouts.
-
-        let serienButton = document.getElementById("serienkopf");
-        let allesumschliessend = document.getElementById("alles");
-        let ueberschriftSerie= document.getElementById("ueberschriftSerie");
-        let ueberschriftMusik= document.getElementById("musikUeberschrift");
-        let bildnummer = "1";
-
-        document.getElementById("aendernButton").addEventListener("click", () =>{
-
-        // Überprüfung welches Thema aktuell ausgewählt ist
-        // Ändern des Hintergrund zum nächsten Thema
-        console.log(bildnummer);
-        if(bildnummer == "1") {
-
-          serienButton.style.backgroundImage="url(./serien/serienTop2.png)";
-          allesumschliessend.style.backgroundImage = "url(./serien/background2.png)";
-          ueberschriftSerie.style.color = "white";
-          ueberschriftMusik.style.color = "black";
-          bildnummer=2;
-          }
-        else if (bildnummer== "2"){
-
-          serienButton.style.backgroundImage="url(./serien/serienTop3.png)";
-          allesumschliessend.style.backgroundImage = "url(./serien/background3.png)";
-          bildnummer=3;
-          }
-        else if (bildnummer== "3"){
-
-          serienButton.style.backgroundImage="url(./serien/serienTop4.png)";
-          allesumschliessend.style.backgroundImage = "url(./serien/background4.png)";
-          bildnummer=4;
-          }
-        else if (bildnummer== "4"){
-
-          serienButton.style.backgroundImage="url(./serien/serienTop5.png)";
-          allesumschliessend.style.backgroundImage = "url(./serien/background5.png)";
-          bildnummer=5;
-          }
-        else if (bildnummer== "5"){
-
-          serienButton.style.backgroundImage="url(./serien/serienTop.png)";
-          allesumschliessend.style.backgroundImage = "url(./serien/background.png)";
-          ueberschriftSerie.style.color = "black";
-          ueberschriftMusik.style.color = "white";
-          bildnummer=1;
-          }
-        })
-;
-    },
     nav: function(ev){
         ev.preventDefault();
         let currentPage = ev.target.getAttribute('data-target');
@@ -89,4 +88,4 @@ const app = {
 
 }
 
-document.addEventListener('DOMContentLoaded', app.init);
+// document.addEventListener('DOMContentLoaded', app.init);
